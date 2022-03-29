23ee50dde35ed47210913ca6da3b5e72e0cfd502
Aren Lloyd Beaño <34330551+sterbenzxc@users.noreply.github.com>
2022-03-29 08:09:46 +0000

    update

--- main.py
+++ main.py
@@ -33,23 +33,24 @@ async def pegasnipe():
   for x in json_data1:
     if str(x["listingIds"][0]) not in printed:
       response = requests.get("https://api-apollo.pegaxy.io/v1/pegas/" + str(x["pegaIds"][0]))
-      json_data = json.loads(response.text)
-      totalstats = float(json_data['speed']) + float(json_data['strength']) + float(json_data['wind']) + float(json_data['fire']) + float(json_data['water']) + float(json_data['lightning'])
-      if(totalstats >= int(env_vars['stats'])):
-        listObj.append({"id": str(x["listingIds"][0]) })
-        embed=discord.Embed(
-        title="**Snipe this PEGA**",
-            url="",
-            description="",
-            color=discord.Color.blue())
-        embed.add_field(name="**Pega ID**", value=str(x["pegaIds"][0]), inline=False)
-        embed.add_field(name="**Blood Line**", value=str(x["bloodLine"]), inline=False)
-        embed.add_field(name="**Total Stats**", value=str(totalstats), inline=False)
-        embed.add_field(name="**Marketplace link**", value="https://play.pegaxy.io/marketplace/listing/" + str(x["listingIds"][0]), inline=False)
-        embed.set_footer(text="Created By: Renzxc#6896")
-        await channel.send(embed=embed)
-        await channel2.send(embed=embed)
-        await channel3.send(embed=embed)
+      if response:
+        json_data = json.loads(response.text)
+        totalstats = float(json_data['speed']) + float(json_data['strength']) + float(json_data['wind']) + float(json_data['fire']) + float(json_data['water']) + float(json_data['lightning'])
+        if(totalstats >= int(env_vars['stats'])):
+          listObj.append({"id": str(x["listingIds"][0]) })
+          embed=discord.Embed(
+          title="**Snipe this PEGA**",
+              url="",
+              description="",
+              color=discord.Color.blue())
+          embed.add_field(name="**Pega ID**", value=str(x["pegaIds"][0]), inline=False)
+          embed.add_field(name="**Blood Line**", value=str(x["bloodLine"]), inline=False)
+          embed.add_field(name="**Total Stats**", value=str(totalstats), inline=False)
+          embed.add_field(name="**Marketplace link**", value="https://play.pegaxy.io/marketplace/listing/" + str(x["listingIds"][0]), inline=False)
+          embed.set_footer(text="Created By: Renzxc#6896")
+          await channel.send(embed=embed)
+          await channel2.send(embed=embed)
+          await channel3.send(embed=embed)
   updatelist(listObj)
 
 @client.event
